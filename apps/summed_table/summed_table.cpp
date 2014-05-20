9ffa6b076c1f2ac16c265535a3c7773d11a2bd6d
Gaurav Chaurasia <gchauras@mit.edu>
2014-05-20 13:17:17 -0400

    Added summed area table example

--- apps/summed_table/summed_table.cpp
+++ apps/summed_table/summed_table.cpp
@@ -1,191 +1,219 @@
 #include <iostream>
-#include <cstdio>
-#include <string>
-#include <cmath>
 #include <Halide.h>
 
-#include <common_inc/timer.h>
-#include <common_inc/filter.h>
-#include <common_inc/utils.h>
+#include "../../lib/split.h"
+
+#define MAX_THREAD 192
 
 using namespace Halide;
 
+using std::vector;
+using std::string;
+using std::cerr;
+using std::endl;
+
+
 int main(int argc, char **argv) {
-    Arguments args("summed_table", argc, argv);
+    Arguments args("recursive_filter", argc, argv);
 
-    int PAD   =  1;
-    int BLOCK = (1<<args.block);
-    int WIDTH = (1<<args.width);
+    bool  verbose = args.verbose;
+    bool  nocheck = args.nocheck;
+    int   width   = args.width;
+    int   height  = args.height;
+    int   tile_width = args.block;
+    int   iterations = args.iterations;
 
-    // ----------------------------------------------------------------------------------------------
+    Image<int> random_image = generate_random_image<int>(width,height);
 
-    Image<float> image = generate_random_image<float>(WIDTH,WIDTH);
-
-    // ----------------------------------------------------------------------------------------------
-
-    ///
-    /// Halide solution
-    ///
-    Var x("x"), y("y"), ix("ix"), iy("iy"), ox("ox"), oy("oy"), tx("tx"), ty("ty");
-
-    /// Number of blocks in image
-    Expr numBlocks = WIDTH/BLOCK;
-
-    /// Reductions do not need to be performed on the padded zone of inner dimensions
-    RDom rx(PAD, BLOCK);
-    RDom ry(PAD, BLOCK);
-
-    ///
-    /// Reduction variables for transferring prologues from one block to
-    /// next, range is determined by number of blocks in x and y
-    ///
-    RDom px(0, numBlocks);
-    RDom py(0, numBlocks);
-
-    ///
-    /// Func definitions for algorithm
-    ///
-    Func Input             ("Input");               ///< Input 4D image
-    Func Prologue_incomp_x ("Prologue_incomp_x");   ///< x causal filter to compute incomplete prologues
-    Func Prologue_incomp_y ("Prologue_incomp_y");   ///< y causal filter to compute incomplete prologues
-    Func Prologue_incomp   ("Prologue_incomp");     ///< Incomplete prologues
-    Func Prologue_comp_x   ("Prologue_comp_x");     ///< Complete prologues along x
-    Func Prologue_comp_y   ("Prologue_comp_y");     ///< Complete prologues along y
-    Func Causal_x          ("Causal_x");            ///< x causal filter to compute final result
-    Func Causal_y          ("Causal_y");            ///< y causal filter to compute final result
-    Func Final             ("Final");               ///< Output 2D image
-
-    ///
-    /// Stage 0: Convert 2D image into 4D.
-    /// Outer dimensions: ox,oy; inner dimensions: ix,iy
-    /// ox = 0 to image_width / block_size
-    /// oy = 0 to image_width/ block_size
-    /// ix = 0 to block_size + 2*PAD
-    /// iy = 0 to block_size + 2*PAD
-    /// Both ix and iy should normally range from 0 to block_size-1.
-    /// But we pad it on either side with zeros to accommodate reductions.
-    ///
-    /// Stage 1: compute the causal filtering within each block
-    /// with no contribution from neighboring blocks - inter-block parallelism to be exploited
-    ///
-    Prologue_incomp_y(ix, iy, ox, oy)  = select(ix>=PAD && ix<=BLOCK+PAD-1 && iy>=PAD && iy<=BLOCK+PAD-1, image(clamp(ox*BLOCK+(ix-PAD),0,-1), clamp(oy*BLOCK+(iy-PAD),0,WIDTH-1)), 0.0f);
-    Prologue_incomp_y(ix, ry, ox, oy) += Prologue_incomp_y(ix,   ry-1, ox, oy);
-    Prologue_incomp_x(ix, iy, ox, oy)  = Prologue_incomp_y(ix,   iy  , ox, oy);
-    Prologue_incomp_x(rx, iy, ox, oy) += Prologue_incomp_x(rx-1, iy  , ox, oy);
-    Prologue_incomp  (ix, x, ox, oy)   = select(x==0, Prologue_incomp_x(ix, PAD+BLOCK-1, ox, oy), Prologue_incomp_x(PAD+BLOCK-1, ix, ox, oy));
-
-    Prologue_incomp_y.compute_at(Prologue_incomp, Var("blockidx"));
-    Prologue_incomp_x.compute_at(Prologue_incomp, Var("blockidx"));
-
-    Prologue_incomp_y.reorder(ix, iy, ox, oy).cuda_threads(ix);
-    Prologue_incomp_x.reorder(iy, ix, ox, oy).cuda_threads(iy);
-
-    Prologue_incomp_y.update().reorder(Var(ry.x.name()), ix, ox, oy).cuda_threads(ix);
-    Prologue_incomp_x.update().reorder(Var(rx.x.name()), iy, ox, oy).cuda_threads(iy);
-
-    Prologue_incomp.compute_root().cuda_blocks(ox,oy).cuda_threads(ix);
-
-    ///
-    /// Stage 2: Compute the incomplete y prologues as last column of each inner block.
-    /// Complete the y prologues oy accumulating prologues of previous block.
-    ///
-    Prologue_comp_y(x, oy)  = 0.0f;
-    Prologue_comp_y(x, py) += Prologue_incomp(x%BLOCK+PAD, 0, x/BLOCK, py) + Prologue_comp_y(x, py-1);
-
-    Prologue_comp_y.compute_root().reorder(oy, x).cuda_tile(x, BLOCK*6);
-    Prologue_comp_y.update().reorder(Var(py.x.name()), x).cuda_tile(x, BLOCK*6);
-
-
-    ///
-    /// Stage 3: Causal the the twice incomplete x prologues as last row of each.
-    /// Complete the twice incomplete x prologues oy accumulating
-    /// - x prologue of previous block along x
-    /// - y prologue of previous block along y
-    ///
-    Prologue_comp_x(y, ox)  = 0.0f;
-    Prologue_comp_x(y, px) += Prologue_incomp(y%BLOCK+PAD, 1, px, y/BLOCK) + Prologue_comp_x(y, px-1) + Prologue_comp_y(px*BLOCK+BLOCK-1, y/BLOCK-1);
-
-    Prologue_comp_x.compute_root().reorder(ox, y).cuda_tile(y, BLOCK*6);
-    Prologue_comp_x.update().reorder(Var(px.x.name()), y).cuda_tile(y, BLOCK*6);
-
-    ///
-    /// Stage 4: Add the completed prologues and epilogues to the initial filtering
-    /// result in Stage 1
-    ///
-    Causal_y(ix, iy, ox, oy)  = select(ix>=PAD && ix<=BLOCK+PAD-1 && iy>=PAD && iy<=BLOCK+PAD-1, image(clamp(ox*BLOCK+(ix-PAD),0,-1), clamp(oy*BLOCK+(iy-PAD),0,WIDTH-1)), 0.0f);
-    Causal_y(ix, ry, ox, oy) += Causal_y(ix,   ry-1, ox, oy);
-    Causal_x(ix, iy, ox, oy)  = Causal_y(ix,   iy  , ox, oy);
-    Causal_x(rx, iy, ox, oy) += Causal_x(rx-1, iy  , ox, oy);
-
-    Final(x,y) = Causal_x(x%BLOCK+PAD, y%BLOCK+PAD, x/BLOCK, y/BLOCK) + Prologue_comp_y(x, y/BLOCK-1) + Prologue_comp_x(y, x/BLOCK-1);
-
-#define PRE_FETCH 1
-#if PRE_FETCH
-    // split iy by 7, because we can only launch 32*6 threads in a block
-    Causal_y.compute_at(Final, Var("blockidx")).split(iy, iy, y, 7).reorder(y, iy, ix, ox, oy).cuda_threads(ix, iy);
-#else
-    Causal_y.compute_at(Final, Var("blockidx")).reorder(iy, ix, ox, oy).cuda_threads(ix);
-#endif
-    Causal_y.update().reorder(Var(ry.x.name()), ix, ox, oy).cuda_threads(ix);
-
-    Causal_x.compute_at(Final, Var("blockidx")).reorder(ix, iy, ox, oy).cuda_threads(iy);
-    Causal_x.update().reorder(Var(rx.x.name()), iy, ox, oy).cuda_threads(iy);
-
-    Final.compute_root().split(y, oy, iy, BLOCK).split(x, ox, ix, BLOCK).reorder(iy, ix, ox, oy).cuda_blocks(ox,oy).cuda_threads(ix);
-    Final.bound(x, 0, WIDTH).bound(y, 0, WIDTH);
+    ImageParam image(type_of<int>(), 2);
+    image.set(random_image);
 
     // ----------------------------------------------------------------------------------------------
 
-    ///
-    /// JIT compilation
-    ///
+    int filter_order_x = 1;
+    int filter_order_y = 1;
 
-    std::cerr << "\nJIT compilation ... " << std::endl;
-    Final.compile_jit();
+    Func I("Input");
+    Func W("Weight");
+    Func S("S");
 
-    Buffer hl_out_buff(type_of<float>(), WIDTH, WIDTH);
-    {
-        Timer t("Running ... ");
-        for (int k=0; k<args.iterations; k++) {
-            Final.realize(hl_out_buff);
-            if (k == args.iterations-1)
-                hl_out_buff.copy_to_host();
-            hl_out_buff.free_dev_buffer();
-        }
+    Var x("x");
+    Var y("y");
+
+    RDom rx(1, image.width()-1, "rx");
+    RDom ry(1, image.height()-1,"ry");
+
+    I(x,y) = select((x<0 || y<0 || x>image.width()-1 || y>image.height()-1), 0, image(clamp(x,0,image.width()-1),clamp(y,0,image.height()-1)));
+
+    W(x, y) = 1;  // filter weights
+
+    S(x, y) = I(x,y);
+    S(rx,y) = S(rx,y) + S(rx-1,y);
+    S(x,ry) = S(x,ry) + S(x,ry-1);
+
+    // ----------------------------------------------------------------------------------------------
+
+    Var xi("xi"), yi("yi");
+    Var xo("xo"), yo("yo");
+
+    RDom rxi(1, tile_width-1, "rxi");
+    RDom ryi(1, tile_width-1, "ryi");
+
+    split(S, W, Internal::vec(0,1),
+            Internal::vec(x,y), Internal::vec(xi,yi), Internal::vec(xo,yo),
+            Internal::vec(rx,ry), Internal::vec(rxi,ryi),
+            Internal::vec(filter_order_x, filter_order_y));
+
+    // ----------------------------------------------------------------------------------------------
+
+    float_dependencies_to_root(S);
+    merge_and_inline(S,
+            "S$split$$Intra2_x$$Intra2_y$",
+            "S$split$$Intra2_x$$Intra_y$",
+            "S$split$$Intra_x$$Intra2_y$",
+            "SIntra_Tail");
+    merge_and_inline(S, "S$split$$Intra_x$$Tail_y$", "S$split$$Intra2_x$$Tail_y$" , "STail_y");
+    merge_and_inline(S, "S$split$$Intra_x$$CTail_y$", "S$split$$Intra2_x$$CTail_y$", "SCTail_y");
+    inline_function (S, "S$split$$Intra2_x$$Deps_y$");
+    inline_function (S, "S$split$$Intra2_x$");
+    swap_variables  (S, "STail_y", xi, yi);
+    merge_and_inline(S, "STail_y", "S$split$$Tail_x$", "STail");
+    inline_function (S, "S$split$$Intra_x$$Deps_y$");
+    inline_function (S, "S$split$$Deps_x$");
+    inline_function (S, "S$split$$Intra_x$");
+    inline_function (S, "S$split$");
+
+    // ----------------------------------------------------------------------------------------------
+
+    vector<Func> func_list;
+    extract_func_calls(S, func_list);
+
+    map<string,Func> functions;
+    for (size_t i=0; i<func_list.size(); i++) {
+        cerr << func_list[i] << endl;
+        functions[func_list[i].name()] = func_list[i];
+    }
+
+    Func S_intra0= functions["SIntra_Tail"];
+    Func S_tails = functions["STail"];
+    Func S_intra = functions["S$split$$Intra_x$$Intra_y$"];
+    Func S_ctailx= functions["S$split$$CTail_x$"];
+    Func S_ctaily= functions["SCTail_y"];
+    Func S_final = functions["S"];
+
+    Target target = get_jit_target_from_environment();
+    if (target.has_gpu_feature() || (target.features & Target::GPUDebug)) {
+        Var t("t");
+
+        S_intra0.compute_at(S_tails, Var("blockidx"));
+        //S_intra0.split(yi,t,yi, MAX_THREAD/WARP_SIZE).reorder(t,xi,yi,xo,yo).gpu_threads(xi,yi).gpu_blocks(xo,yo);
+        S_intra0.reorder_storage(xi,yi,xo,yo);
+        S_intra0.reorder(xi,yi,xo,yo).gpu_threads(yi);
+        S_intra0.update(0).reorder(rxi.x,yi,xo,yo).gpu_threads(yi);
+        S_intra0.update(1).reorder(ryi.x,xi,xo,yo).gpu_threads(xi);
+
+        S_tails.compute_root();
+        S_tails.reorder_storage(yi,xi,xo,yo);
+        //S_tails.split(yi,t,yi, MAX_THREAD/WARP_SIZE).reorder(t,xi,yi,xo,yo);
+        S_tails.reorder(xi,yi,xo,yo);
+        S_tails.gpu_blocks(xo,yo).gpu_threads(xi);
+
+        S_ctaily.compute_root();
+        S_ctailx.reorder_storage(yi,yo,xi,xo);
+        S_ctaily.split(xo,xo,t,MAX_THREAD/tile_width);
+        S_ctaily.reorder(yo,yi,xi,t,xo).gpu_blocks(xo).gpu_threads(xi,t);
+        S_ctaily.update().split(xo,xo,t,MAX_THREAD/tile_width);
+        S_ctaily.update().reorder(xi,t,xo).gpu_blocks(xo).gpu_threads(xi,t);
+
+        S_ctailx.compute_root();
+        S_ctailx.reorder_storage(yi,yo,xi,xo);
+        S_ctailx.split(yo,yo,t,MAX_THREAD/tile_width);
+        S_ctailx.reorder(xo,xi,yi,t,yo).gpu_blocks(yo).gpu_threads(yi,t);
+        S_ctailx.update().split(yo,yo,t,MAX_THREAD/tile_width);
+        S_ctailx.update().reorder(yi,t,yo).gpu_blocks(yo).gpu_threads(yi,t);
+
+        S_intra.compute_at(S_final, Var("blockidx"));
+        S_intra.reorder_storage(xi,yi,xo,yo);
+        //S_intra.split(yi,t,yi, MAX_THREAD/tile_width).reorder(t,xi,yi,xo,yo).gpu_threads(xi,yi);
+        S_intra.reorder(xi,yi,xo,yo).gpu_threads(yi);
+        S_intra.update(0).reorder(rxi.x,yi,xo,yo).gpu_threads(yi);
+        S_intra.update(1).reorder(ryi.x,xi,xo,yo).gpu_threads(xi);
+
+        S_final.compute_root();
+        S_final.split(x, xo,xi, tile_width).split(y, yo,yi, tile_width);
+        //S_final.split(yi,t,yi, MAX_THREAD/tile_width).reorder(t,xi,yi,xo,yo);
+        S.reorder(yi, xi, xo, yo);
+        S_final.gpu_blocks(xo,yo).gpu_threads(xi);
+        S_final.bound(x, 0, image.width()).bound(y, 0, image.height());
+    }
+    else {
+        cerr << "Warning: No CPU scheduling" << endl;
     }
 
     // ----------------------------------------------------------------------------------------------
 
+    cerr << "\nJIT compilation ... " << endl;
+    S.compile_jit();
 
-    ///
-    /// Check the difference
-    ///
+    Buffer hl_out_buff(type_of<int>(), width,height);
+    {
+        Timer t("Running ... ");
+        for (int k=0; k<iterations; k++) {
+            S.realize(hl_out_buff);
+            if (k < iterations-1) {
+                hl_out_buff.free_dev_buffer();
+            }
+        }
+    }
+    hl_out_buff.copy_to_host();
+    hl_out_buff.free_dev_buffer();
 
-    if (!args.nocheck || args.verbose) {
-        Timer t("Naive CPU serial computation");
+    // ----------------------------------------------------------------------------------------------
 
-        Image<float> ref = reference_recursive_filter<float>(image);
+    if (!nocheck) {
+        cerr << "\nChecking difference ... " << endl;
+        Image<int> hl_out(hl_out_buff);
+        Image<int> diff(width,height);
+        Image<int> ref(width,height);
 
-        Image<float> hl_out(hl_out_buff);
-        Image<float> diff(WIDTH,WIDTH);
-        float diff_sum = 0;
-        for (int y=0; y<WIDTH; y++) {
-            for (int x=0; x<WIDTH; x++) {
-                diff(x,y) = std::abs(ref(x,y) - hl_out(x,y));
-                diff_sum += diff(x,y);
+        for (int y=0; y<height; y++) {
+            for (int x=0; x<width; x++) {
+                ref(x,y) = random_image(x,y);
+            }
+        }
+        for (int y=0; y<height; y++) {
+            for (int x=1; x<width; x++) {
+                ref(x,y) += ref(x-1,y);
+            }
+        }
+        for (int y=1; y<height; y++) {
+            for (int x=0; x<width; x++) {
+                ref(x,y) += ref(x,y-1);
             }
         }
 
-        if (args.verbose) {
-            std::cerr << "Input" << std::endl << image << std::endl;
-            std::cerr << "Reference" << std::endl << ref << std::endl;
-            std::cerr << "Halide output" << std::endl << hl_out << std::endl;
-            std::cerr << "Difference" << std::endl << diff << std::endl;
+        int diff_sum = 0;
+        int all_sum = 0;
+        for (int y=0; y<height; y++) {
+            for (int x=0; x<width; x++) {
+                diff(x,y) = std::abs(ref(x,y) - hl_out(x,y));
+                diff_sum += diff(x,y);
+                all_sum += ref(x,y);
+            }
+        }
+        float diff_ratio = 100.0f * float(diff_sum)/float(all_sum);
+
+        if (verbose) {
+            cerr << "Input" << endl << random_image << endl;
+            cerr << "Reference" << endl << ref << endl;
+            cerr << "Halide output" << endl << hl_out << endl;
+            cerr << "Difference " << endl << diff << endl;
+            cerr << "\nError = " << diff_sum << " ( " << diff_ratio << "% )" << endl;
         } else {
-            std::cerr << "\nDifference = " << diff_sum << std::endl;
-            std::cerr << std::endl;
+            cerr << "\nError = " << diff_sum << " ( " << diff_ratio << "% )" << endl;
+            cerr << endl;
         }
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
