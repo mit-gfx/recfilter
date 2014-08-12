7d0ed35e27699a3dcd59f3f80e2691b0d82a29ef
Gaurav Chaurasia <gchauras@mit.edu>
2014-08-12 18:30:53 -0400

    Added scripts to run OpenCL and CUDA profiling

---
+++ apps/opencl_profile.sh
@@ -0,0 +1,22 @@
+#/usr/bin/bash
+
+if [ $# -ge 1 ]
+then
+    f=/tmp/$RANDOM
+    HL_JIT_TARGET=opencl-gpu_debug $@ 2> $f
+
+    if [ $? == 0 ]
+    then
+        cat $f | \
+            sed '/halide_dev_run\|Time/!d' | \
+            sed '/halide_dev_run/,+1!d' | \
+            sed 's/CL.*entry: //g' | \
+            sed ':a;N;$!ba;s/\n /;\t/g' | \
+            sed 's/blocks: \|threads: \|shmem: \|Time: /;\t/g' | \
+            sed 's/,\|)//g'
+    else
+        cat $f
+    fi
+else
+    echo 'Usage: ./opencl_profile [application_command_line]'
+fi
