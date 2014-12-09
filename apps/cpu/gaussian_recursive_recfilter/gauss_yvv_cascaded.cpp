6405811d029916e4e05655653cb0a08629edb71e
Gaurav Chaurasia <gchauras@mit.edu>
2014-12-08 20:27:54 -0500

    Provisional recfilter version of CPU recursive Gaussians; no schedules

---
+++ apps/cpu/gaussian_recursive_recfilter/gauss_yvv_cascaded.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+#include <Halide.h>
+
+#include "gaussian_weights.h"
+#include "recfilter.h"
+
+#define MAX_THREAD 192
+
+using namespace Halide;
+
+using std::vector;
+using std::string;
+using std::cerr;
+using std::endl;
+
+int main(int argc, char **argv) {
+    Arguments args(argc, argv);
+
+    bool nocheck = args.nocheck;
+    int  width   = args.width;
+    int  height  = args.width;
+    int  tile_width = args.block;
+
+    float sigma = 16.0f;
+
+    Image<float> random_image = generate_random_image<float>(width,height);
+
+    ImageParam image(type_of<float>(), 2);
+    image.set(random_image);
+
+    // ----------------------------------------------------------------------------------------------
+
+    vector<float> W1 = gaussian_weights(sigma, 1);
+    vector<float> W2 = gaussian_weights(sigma, 2);
+
+    RecFilterDim x("x", width);
+    RecFilterDim y("y", height);
+
+    RecFilter filter;
+    filter.set_clamped_image_border();
+
+    filter(x, y) = image(clamp(x,0,image.width()-1), clamp(y,0,image.height()-1));
+
+    filter.add_filter(+x, W1);
+    filter.add_filter(-x, W1);
+    filter.add_filter(+y, W1);
+    filter.add_filter(-y, W1);
+    filter.add_filter(+x, W2);
+    filter.add_filter(-x, W2);
+    filter.add_filter(+y, W2);
+    filter.add_filter(-y, W2);
+
+    // cascade the scans
+    vector<RecFilter> cascaded_filters = filter.cascade({0,1,2,3}, {4,5,6,7});
+
+    RecFilter filter1 = cascaded_filters[0];
+    RecFilter filter2 = cascaded_filters[1];
+
+    filter1.split(x, tile_width, y, tile_width);
+    filter2.split(x, tile_width, y, tile_width);
+
+    cerr << filter2 << endl;
+
+    // ----------------------------------------------------------------------------------------------
+
+    {
+    }
+
+    // ----------------------------------------------------------------------------------------------
+
+    cerr << "\nJIT compilation ... " << endl;
+    filter2.func().compile_jit();
+
+    Buffer hl_out_buff(type_of<float>(), width,height);
+    {
+        filter2.func().realize(hl_out_buff);
+    }
+    hl_out_buff.copy_to_host();
+    hl_out_buff.free_dev_buffer();
+
+    // ----------------------------------------------------------------------------------------------
+
+    if (!nocheck) {
+        cerr << "\nChecking difference ...\n" << endl;
+        Image<float> hl_out(hl_out_buff);
+        Image<float> ref = reference_gaussian(random_image, sigma);
+        cerr << "Difference with true Gaussian \n" << CheckResult<float>(ref,hl_out) << endl;
+    }
+
+    return 0;
+}
---
+++ apps/cpu/gaussian_recursive_recfilter/gauss_yvv_direct.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+#include <Halide.h>
+
+#include "gaussian_weights.h"
+#include "recfilter.h"
+
+#define MAX_THREAD 192
+
+using namespace Halide;
+
+using std::vector;
+using std::string;
+using std::cerr;
+using std::endl;
+
+int main(int argc, char **argv) {
+    Arguments args(argc, argv);
+
+    bool nocheck = args.nocheck;
+    int  width   = args.width;
+    int  height  = args.width;
+    int  tile_width = args.block;
+
+    float sigma = 16.0f;
+
+    Image<float> random_image = generate_random_image<float>(width,height);
+
+    ImageParam image(type_of<float>(), 2);
+    image.set(random_image);
+
+    // ----------------------------------------------------------------------------------------------
+
+    vector<float> W3 = gaussian_weights(sigma, 3);
+
+    RecFilterDim x("x", width);
+    RecFilterDim y("y", height);
+
+    RecFilter filter;
+
+    filter.set_clamped_image_border();
+
+    filter(x, y) = image(clamp(x,0,image.width()-1), clamp(y,0,image.height()-1));
+
+    filter.add_filter(+x, W3);
+    filter.add_filter(-x, W3);
+    filter.add_filter(+y, W3);
+    filter.add_filter(-y, W3);
+
+    filter.split(x, tile_width, y, tile_width);
+
+    cerr << filter << endl;
+
+    // ----------------------------------------------------------------------------------------------
+
+    {
+    }
+
+    // ----------------------------------------------------------------------------------------------
+
+    cerr << "\nJIT compilation ... " << endl;
+    filter.func().compile_jit();
+
+    Buffer hl_out_buff(type_of<float>(), width,height);
+    {
+        filter.func().realize(hl_out_buff);
+    }
+    hl_out_buff.copy_to_host();
+    hl_out_buff.free_dev_buffer();
+
+    // ----------------------------------------------------------------------------------------------
+
+    if (!nocheck) {
+        cerr << "\nChecking difference ...\n" << endl;
+        Image<float> hl_out(hl_out_buff);
+        Image<float> ref = reference_gaussian(random_image, sigma);
+        cerr << "Difference with true Gaussian \n" << CheckResultVerbose<float>(ref,hl_out) << endl;
+    }
+
+    return 0;
+}
