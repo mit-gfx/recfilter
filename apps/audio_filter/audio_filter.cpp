e556e5e614b351412e445ab9fa1f7ef4e3509745
Gaurav Chaurasia <gchauras@mit.edu>
2015-01-03 16:27:42 -0500

    Minor changes

--- apps/audio_filter/audio_filter.cpp
+++ apps/audio_filter/audio_filter.cpp
@@ -5,13 +5,11 @@
 
 using namespace Halide;
 
-using std::vector;
-using std::string;
 using std::cerr;
 using std::endl;
 
 #define CHANNELS 1
-#define ORDER    1
+#define ORDER    3
 
 int main(int argc, char **argv) {
     Arguments args(argc, argv);
@@ -22,69 +20,41 @@ int main(int argc, char **argv) {
 
     Image<float> image = generate_random_image<float>(width,CHANNELS);
 
-    vector<double> coeffs(ORDER+1, 1);
+    std::vector<double> coeffs(ORDER+1, 1);
     coeffs[0] = 1.0;
 
-    cerr << "Order = " << ORDER << ", array size = " << width << " channels = " << CHANNELS << endl;
+    Buffer out;
 
-    // Tiled implementation
+    double time1, time2;
+
+    RecFilterDim x("x", width);
+    RecFilterDim c("c", CHANNELS);
+
+    // non-tiled implementation
     {
-        Buffer out;
-
-        RecFilterDim x("x", width);
-        RecFilterDim c("c", CHANNELS);
-
-        RecFilter F;
-        F(x,c) = image(clamp(x,0,width-1), c);
+        RecFilter F("R_nontiled");
+        F(x,c) = image(x,c);
         F.add_filter(+x, coeffs);
-
-        if (F.target().has_gpu_feature()) {
-            cerr << "Filter only designed for CPU, change HL_JIT_TARGET to CPU target" << endl;
-            assert(false);
-        }
-
         F.intra_schedule().compute_globally();
         F.compile_jit("nontiled.html");
-        double time1 = F.realize(out, iterations);
+        time1 = F.realize(out, iterations);
+    }
 
+    // tiled implementation
+    {
+        RecFilter F("R_tiled");
+        F(x,c) = image(x,c);
+        F.add_filter(+x, coeffs);
         F.split(x, tile_width);
         F.intra_schedule().compute_locally() ;
         F.inter_schedule().compute_globally();
         F.compile_jit("tiled.html");
-        double time2 = F.realize(out, iterations);
-
-        cerr << "Naive: " << time1 << " ms" << endl;
-        cerr << "Tiled: " << time2 << " ms" << endl;
+        time2 = F.realize(out, iterations);
     }
-    return 0;
 
-    // C++ non tiled implementation
-    {
-        unsigned long start, end, total;
-        int w = image.width();
-        int c = CHANNELS;
-
-        vector<float> out(image.width()*CHANNELS, 0.0f);
-        for (int n=0; n<iterations; n++) {
-            std::copy(image.data(), image.data()+w*c, out.begin());
-
-            start = RecFilter::millisecond_timer();
-#pragma omp parallel for
-            for (int j=0; j<c; j++) {
-                for (int i=0; i<w; i++) {
-                    float temp = 0.0f;
-                    for (int k=0; k<ORDER+1; k++) {
-                        temp += (i-k>=0 ? coeffs[k]*out[j*w+i-k] : 0.0f);
-                    }
-                    out[j*w+i] = temp;
-                }
-            }
-            end = RecFilter::millisecond_timer();
-            total += (end-start);
-        }
-
-        cerr << "C++ naive: " << double(total)/double(iterations) << " ms" << endl;
-    }
+    std::cerr << "\nOrder = " << ORDER << ", array = (" << width << ", " << CHANNELS << ")\n"
+              << "Naive: " << time1 << " ms\n"
+              << "Tiled: " << time2 << " ms\n" << std::endl;
 
     return 0;
 }
