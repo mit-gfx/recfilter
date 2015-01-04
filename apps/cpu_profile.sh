6f5acb06581fa6b9da67dd2a9036d68055e90e04
Gaurav Chaurasia <gchauras@mit.edu>
2015-01-03 19:41:21 -0500

    Added script to profile CPU applications

---
+++ apps/cpu_profile.sh
@@ -0,0 +1,23 @@
+#/usr/bin/bash
+
+TFILE=$( mktemp )
+
+if [ -z "$TFILE1" ]
+then
+    TFILE="$(basename $0).$RANDOM.tmp"
+fi
+
+HALIDE_DIR=../halide
+HL_PROFILER=$HALIDE_DIR/bin/HalideProf
+
+if [ $# -ge 1 ]
+then
+    HL_PROFILE=1 HL_JIT_TARGET=x86-64 $@ 2> $TFILE
+
+    $HL_PROFILER -top 1000000 -sort t < $TFILE
+
+    rm -f $TFILE
+
+else
+    echo 'Usage: ./cpu_profile [application_command_line]'
+fi
