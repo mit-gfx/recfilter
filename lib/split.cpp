5b55398aab59db131e776badb35015c5f8b7914a
Gaurav Chaurasia <gchauras@mit.edu>
2015-01-04 16:45:44 -0500

    Removed the older bank conflict management code

--- lib/split.cpp
+++ lib/split.cpp
@@ -68,8 +68,7 @@ static map<string, RecFilterFunc> recfilter_func_list;
 
 // -----------------------------------------------------------------------------
 
-/** Convert the pure def into the first update def and leave the pure def
- * undefined
+/** Convert the pure def into the first update def and leave the pure def undefined
  * \param[in,out] rF function to be modified
  * \param[in] split_info tiling metadata
  */
@@ -80,6 +79,11 @@ static void convert_pure_def_into_first_update_def(
     assert(!split_info.empty());
 
     Function F = rF.func;
+
+    // nothing needed if the function is pure
+    if (F.is_pure()) {
+        return;
+    }
 
     vector<string> pure_args = F.args();
     vector<Expr> values = F.values();
@@ -516,60 +520,6 @@ static RecFilterFunc extract_tails_from_each_scan(
     }
 
     return rF_intra_tail;
-}
-
-// -----------------------------------------------------------------------------
-
-/** Bank conflicts expected if there are one or more single tiled scan; add an
- * extra update def which assigns an undefined value to column of pixels beyond
- * the max in the first dimension; to pad the shared mem and avoid bank conflicts
- */
-static void add_padding_to_avoid_bank_conflicts(Function F, vector<SplitInfo> split_info) {
-    string var;
-    int tile_width   = 0;
-    int filter_order = 0;
-    int num_scans    = 0;
-    int s_id         = -1;
-
-    // find the inner most dimension of shared mem storage
-    // that is also the inner var of one of the splits
-    for (int i=0; s_id<0 && i<F.args().size(); i++) {
-        string arg = F.args()[i];
-        for (int j=0; s_id<0 && j<split_info.size(); j++) {
-            if (arg == split_info[j].inner_var.name()) {
-                s_id = j;
-            }
-        }
-    }
-
-    // find if the first dimension has a tiled scan
-    if (s_id>=0 && split_info[ s_id ].num_scans>0) {
-        var         = split_info[ s_id ].inner_var.name();
-        tile_width  = split_info[ s_id ].tile_width;
-        num_scans   = split_info[ s_id ].num_scans;
-        filter_order= split_info[ s_id ].filter_order;
-
-        // add an extra padding of 1
-        int shared_mem_width = tile_width + filter_order*num_scans;
-        int padding = shared_mem_width + 1;
-
-        // map inner var of first dimension to the extent computed above
-        // map inner vars of all other dimensions to 0
-        vector<Expr> args;
-        for (int i=0; i<F.args().size(); i++) {
-            string a = F.args()[i];
-            if (a==var) {
-                args.push_back(padding);
-            } else {
-                args.push_back(Var(a));
-            }
-        }
-
-        vector<Expr> undef_values(F.outputs(), undef(split_info[0].type));
-        F.define_update(args, undef_values);
-    }
-
-    // no need for scheduling tags for the last update def
 }
 
 // -----------------------------------------------------------------------------
@@ -1968,14 +1918,8 @@ void RecFilter::finalize(void) {
         // add padding to intra tile terms to avoid bank conflicts
         for (fit=contents.ptr->func.begin(); fit!=contents.ptr->func.end(); fit++) {
             RecFilterFunc& rF = fit->second;
-            if (rF.func_category==INTRA_1 && rF.func.has_update_definition()) {
+            if (rF.func_category==INTRA_1 || rF.func_category==INTRA_N) {
                 convert_pure_def_into_first_update_def(rF, recfilter_split_info);
-            }
-            if (rF.func_category==INTRA_N && rF.func.has_update_definition()) {
-                convert_pure_def_into_first_update_def(rF, recfilter_split_info);
-                if (contents.ptr->target.has_gpu_feature()) {
-                    add_padding_to_avoid_bank_conflicts(rF.func, recfilter_split_info);
-                }
             }
         }
     }
