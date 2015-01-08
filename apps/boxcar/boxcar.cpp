3c8ca5f966301931f9d45b295252741dddadc9b0
Gaurav Chaurasia <gchauras@mit.edu>
2015-01-08 02:29:01 -0500

    Schedule for single box filter

--- apps/boxcar/boxcar.cpp
+++ apps/boxcar/boxcar.cpp
@@ -3,82 +3,124 @@
 
 #include "recfilter.h"
 
-#define MAX_THREAD        192
-#define BOX_FILTER_FACTOR 16
-
 using namespace Halide;
 
 using std::vector;
 using std::string;
 using std::cerr;
+using std::cout;
 using std::endl;
 
-
 int main(int argc, char **argv) {
+    // radius of box filter
+    const int B = 10;
+
     Arguments args(argc, argv);
+    bool nocheck   = args.nocheck;
+    int iter       = args.iterations;
+    int tile_width = args.block;
+    int min_w      = args.min_width;
+    int max_w      = args.max_width;
+    int inc_w      = tile_width;
 
-    bool  nocheck = args.nocheck;
-    int   width   = args.width;
-    int   height  = args.width;
-    int   tile_width = args.block;
-    int   iterations = args.iterations;
+    // Profile the filter for all image widths
+    for (int in_w=min_w; in_w<=max_w; in_w+=inc_w) {
+        int width = in_w;
+        int height= in_w;
 
-    Image<float> random_image = generate_random_image<float>(width,height);
+        Image<float> I = generate_random_image<float>(width,height);
 
-    ImageParam image(type_of<float>(), 2);
-    image.set(random_image);
+        Var u("u"), xo("xo"), xi("xi"), xii("xii");
+        Var v("v"), yo("yo"), yi("yi"), yii("yii");
 
-    // ----------------------------------------------------------------------------------------------
+        RecFilterDim x("x", width);
+        RecFilterDim y("y", height);
 
-    int box = BOX_FILTER_FACTOR;        // radius of box filter
+        Func      S("S");
+        RecFilter F("Box");
 
-    RecFilterDim x("x", width);
-    RecFilterDim y("y", height);
+        S(u,v) = I(min(u+B,width-1), min(v+B,height-1))
+               + I(max(u-B-1,0),     max(v-B-1,0))
+               - I(min(u+B,width-1), max(v-B-1,0))
+               - I(max(u-B-1,0),     min(v+B,height-1));
 
-    RecFilter filter;
-    filter(x,y) = image(x,y);
+        F(x,y) = S(x,y);
+        F.add_filter(x, {1.0, 1.0});
+        F.add_filter(y, {1.0, 1.0});
 
-    filter.add_filter(+x, {1.0, 1.0});
-    filter.add_filter(+y, {1.0, 1.0});
+        F.split(x, tile_width, y, tile_width);
 
-    Func S = filter.func();
+        // ---------------------------------------------------------------------
 
-    Func B("Boxcar");
-    B(x,y) = S(min(x+box,image.width()-1), min(y+box,image.height()-1))
-           + select(x-box-1<0 || y-box-1<0, 0, S(max(x-box-1,0), max(y-box-1,0)))
-           - select(y-box-1<0, 0, S(min(x+box,image.width()-1), max(y-box-1,0)))
-           - select(x-box-1<0, 0, S(max(x-box-1,0), min(y+box,image.height()-1)));
+        int tiles_per_warp = 2;
+        int unroll_w       = 8;
 
-    filter.split(x, tile_width, y, tile_width);
+        S.compute_root()
+            .split(u, xo, xi, 32)
+            .split(v, yo, yi, 32)
+            .split(yi,yi, yii,4).unroll(yii)
+            .reorder(yii,xi,yi,xo,yo)
+            .gpu(xo,yo,xi,yi);
 
-    cerr << filter << endl;
+        F.intra_schedule(1).compute_locally()
+            .reorder_storage(F.inner(), F.outer())
+            .unroll         (F.inner_scan())
+            .split          (F.inner(1), unroll_w)
+            .unroll         (F.inner(1).split_var())
+            .reorder        (F.inner_scan(), F.inner(1).split_var(), F.inner(), F.outer())
+            .gpu_threads    (F.inner(0), F.inner(1))
+            .gpu_blocks     (F.outer(0), F.outer(1));
 
-    // ----------------------------------------------------------------------------------------------
+        F.intra_schedule(2).compute_locally()
+            .reorder_storage(F.tail(), F.inner(), F.outer())
+            .unroll         (F.inner_scan())
+            .split          (F.outer(0), tiles_per_warp)
+            .reorder        (F.inner_scan(), F.tail(), F.outer(0).split_var(), F.inner(), F.outer())
+            .gpu_threads    (F.inner(0), F.outer(0).split_var())
+            .gpu_blocks     (F.outer(0), F.outer(1));
 
-    {
-    }
+        F.inter_schedule().compute_globally()
+            .reorder_storage(F.inner(), F.tail(), F.outer())
+            .unroll         (F.outer_scan())
+            .split          (F.outer(0), tiles_per_warp)
+            .reorder        (F.outer_scan(), F.tail(), F.outer(0).split_var(), F.inner(), F.outer())
+            .gpu_threads    (F.inner(0), F.outer(0).split_var())
+            .gpu_blocks     (F.outer(0));
 
-    Realization out = B.realize();
+        cerr << F << endl;
+        F.compile_jit("stmt.html");
 
-    // ----------------------------------------------------------------------------------------------
+        float time = F.profile(iter);
 
-    if (!nocheck) {
-        cerr << "\nChecking difference ... " << endl;
-        Image<float> hl_out(out);
-        Image<float> ref(width,height);
+        cerr << "Width " << width << " " << time << " ms" << endl;
 
-        for (int y=0; y<height; y++) {
-            for (int x=0; x<width; x++) {
-                ref(x,y) = 0;
-                for (int v=y-box; v<=y+box; v++) {
-                    for (int u=x-box; u<=x+box; u++) {
-                        if (v>=0 && u>=0 && v<height && u<width)
-                            ref(x,y) += random_image(u,v);
-                    }
+        // ---------------------------------------------------------------------
+
+        if (!nocheck) {
+            cerr << "\nChecking difference ... " << endl;
+            Realization out = F.realize();
+
+            Image<float> hl_out(out);
+            Image<float> ref(width,height);
+
+            for (int y=0; y<height; y++) {
+                for (int x=0; x<width; x++) {
+                    ref(x,y) = I(x,y);
                 }
             }
+            for (int y=0; y<height; y++) {
+                for (int x=1; x<width; x++) {
+                    ref(x,y) += ref(x-1,y);
+                }
+            }
+            for (int y=1; y<height; y++) {
+                for (int x=0; x<width; x++) {
+                    ref(x,y) += ref(x,y-1);
+                }
+            }
+
+            cout << CheckResult<float>(ref,hl_out) << endl;
         }
-        cerr << CheckResult<float>(ref,hl_out) << endl;
     }
 
     return 0;
