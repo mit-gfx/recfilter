00bb958cd4794a14a30e69ed8c3540f24d4d265b
Gaurav Chaurasia <gchauras@mit.edu>
2015-01-11 22:06:08 -0500

    Bugfixes in 1D filter

--- apps/audio_filter/audio_filter.cpp
+++ apps/audio_filter/audio_filter.cpp
@@ -3,58 +3,64 @@
 
 #include "recfilter.h"
 
+#define MAX_ORDER 50
+
 using namespace Halide;
 
+using std::vector;
 using std::cerr;
 using std::endl;
-
-#define CHANNELS 1
-#define ORDER    3
 
 int main(int argc, char **argv) {
     Arguments args(argc, argv);
 
-    int width      = args.width;
+    int width     = args.width;
     int tile_width = args.block;
     int iterations = args.iterations;
 
-    Image<float> image = generate_random_image<float>(width,CHANNELS);
+    Image<float> image = generate_random_image<float>(width);
 
-    std::vector<float> coeffs(ORDER+1, 1);
-    coeffs[0] = 1.0;
+    vector<float> time_naive;
+    vector<float> time_tiled;
 
-    Buffer out;
+    for (int order=1; order<MAX_ORDER; order++) {
+        std::vector<float> coeffs(order+1, 0.01);
+        coeffs[0] = 1.0;
 
-    float time1, time2;
+        Buffer out;
 
-    RecFilterDim x("x", width);
-    RecFilterDim c("c", CHANNELS);
+        RecFilterDim x("x", width);
 
-    // non-tiled implementation
-    {
-        RecFilter F("R_nontiled");
-        F(x,c) = image(x,c);
-        F.add_filter(+x, coeffs);
-        F.intra_schedule().compute_globally();
-        F.compile_jit("nontiled.html");
-        time1 = F.profile(iterations);
+        // non-tiled implementation
+        {
+            RecFilter F("R_nontiled");
+            F(x) = image(clamp(x,0,width-1));
+            F.add_filter(+x, coeffs);
+            F.compute_globally();
+            F.compile_jit("nontiled.html");
+            time_naive.push_back(F.profile(iterations));
+        }
+
+        // tiled implementation
+        {
+            int vw = 8;
+
+            RecFilter F("R_tiled");
+            F(x) = image(clamp(x,0,width-1));
+            F.add_filter(+x, coeffs);
+            F.split(x, tile_width);
+
+            F.intra_schedule().compute_locally() .vectorize(F.inner(0),vw).parallel(F.outer(0));
+            F.inter_schedule().compute_globally().vectorize(F.inner(0),vw);
+
+            F.compile_jit("tiled.html");
+            time_tiled.push_back(F.profile(iterations));
+        }
+
+        cerr << "Order=" << order << " "
+             << "naive " << time_naive[time_naive.size()-1] << " "
+             << "tiled " << time_tiled[time_tiled.size()-1] << " ms" << endl;
     }
-
-    // tiled implementation
-    {
-        RecFilter F("R_tiled");
-        F(x,c) = image(x,c);
-        F.add_filter(+x, coeffs);
-        F.split(x, tile_width);
-        F.intra_schedule().compute_locally() ;
-        F.inter_schedule().compute_globally();
-        F.compile_jit("tiled.html");
-        time2 = F.profile(iterations);
-    }
-
-    std::cerr << "\nOrder = " << ORDER << ", array = (" << width << ", " << CHANNELS << ")\n"
-              << "Naive: " << time1 << " ms\n"
-              << "Tiled: " << time2 << " ms\n" << std::endl;
 
     return 0;
 }
