6db214c0b86fa8698dccbac458aad81008151cf0
Gaurav Chaurasia <gchauras@mit.edu>
2015-01-13 11:57:38 -0500

    Fixed compilation error

---
+++ lib/timing.cpp
@@ -0,0 +1,35 @@
+#include "timing.h"
+
+float throughput(float runtime, int pixels) {
+    int gibipixels = 2^30;
+    return float(pixels) / float(gibipixels*runtime*1000.0f);
+}
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+unsigned long millisecond_timer(void) {
+    static SYSTEMTIME t;
+    GetSystemTime(&t);
+    return (unsigned long)((unsigned long)t.wMilliseconds
+            + 1000*((unsigned long)t.wSecond
+                + 60*((unsigned long)t.wMinute
+                    + 60*((unsigned long)t.wHour
+                        + 24*(unsigned long)t.wDay))));
+}
+#elif defined(_APPLE_) || defined(__APPLE__) || \
+    defined(APPLE)   || defined(_APPLE)    || defined(__APPLE) || \
+defined(unix)    || defined(__unix__)  || defined(__unix)
+#include <unistd.h>
+#include <sys/time.h>
+unsigned long millisecond_timer(void) {
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (unsigned long)(t.tv_usec/1000 + t.tv_sec*1000);
+}
+#else
+unsigned long millisecond_timer(void) {
+    std::cerr << "Warning: no timer implementation available" << std::endl;
+    return 0;
+}
+#endif
--- lib/timing.h
+++ lib/timing.h
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <cstdlib>
 #include <string>
 
 /** Logging utility */
@@ -15,7 +16,7 @@ public:
             out.open(filename, std::ios_base::out);
             if (!out.is_open()) {
                 std::cerr << "Could not open " << filename << " for logging" << std::endl;
-                assert(false);
+                exit(EXIT_FAILURE);
             }
         }
     }
@@ -34,10 +35,7 @@ public:
  * \param pixels number of pixels
  * \returns throughput in GiP/s
  */
-float throughput(float runtime, int pixels) {
-    int gibipixels = 2^30;
-    return float(pixels) / float(gibipixels*runtime*1000.0f);
-}
+float throughput(float runtime, int pixels);
 
 /**
  * Millisecond-precision timer function
@@ -52,34 +50,6 @@ float throughput(float runtime, int pixels) {
  * Preprocessor symbols are checked in attempt to detect whether the platform
  * is POSIX or Windows or Mac and defines millisecond_timer() accordingly.
  */
-#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-unsigned long millisecond_timer(void) {
-    static SYSTEMTIME t;
-    GetSystemTime(&t);
-    return (unsigned long)((unsigned long)t.wMilliseconds
-            + 1000*((unsigned long)t.wSecond
-                + 60*((unsigned long)t.wMinute
-                    + 60*((unsigned long)t.wHour
-                        + 24*(unsigned long)t.wDay))));
-}
-#elif defined(_APPLE_) || defined(__APPLE__) || \
-    defined(APPLE)   || defined(_APPLE)    || defined(__APPLE) || \
-defined(unix)    || defined(__unix__)  || defined(__unix)
-#include <unistd.h>
-#include <sys/time.h>
-unsigned long millisecond_timer(void) {
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    return (unsigned long)(t.tv_usec/1000 + t.tv_sec*1000);
-}
-#else
-unsigned long millisecond_timer(void) {
-    std::cerr << "Warning: no timer implementation available" << std::endl;
-    return 0;
-}
-#endif
-
+unsigned long millisecond_timer(void);
 
 #endif // _TIMING_H_
